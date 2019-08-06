1fbd5430b68195ca3d282e0333e2cc3737c88c27
Olga <violetiodum@gmail.com>
2019-08-06 18:47:04 +0300

    added portfolio store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import stocks from './modules/Stocks.js'
+import portfolio from './modules/Portfolio.js'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     modules: {
-        stocks
+        stocks,
+        portfolio
     }
 });
